b1beb290540547a680748952dec04dc0a247bca7
Sabrina Guimarães <ssousagu@emeal.nttdata.com>
2024-04-07 17:40:26 -0300

    init python career track

---
+++ introduction_to_python/script.py
@@ -0,0 +1,42 @@
+# The savings variable you created in the previous exercise with a value of 100 is available to you.
+savings = 100 
+
+# Create the variables monthly_savings and monthly_savings
+monthly_savings = 10
+num_months = 4 
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Add new_savings to your savings
+total_savings = savings + new_savings
+
+# Print the value of total_savings.
+print(total_savings)
+
+# Create a variable half
+half = 0.5 
+
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
+# Guess the type
+"""
+In [1]:
+type(a)
+Out[1]:
+float
+
+In [2]:
+type(b)
+Out[2]:
+str
+
+In [3]:
+type(c)
+Out[3]:
+bool
+"""
